25a5010e7de0d0cea430931ff0544ab9696fdda1
dorltcheng <tin.cheng19@imperial.ac.uk>
2021-11-28 01:29:18 +0000

    1.1

--- src/main/java/dorchengServlet.java
+++ src/main/java/dorchengServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class dorchengServlet extends HttpServlet {
@@ -17,6 +18,9 @@ public class dorchengServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
     IOException {
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client!"+reqBody);
     }
 
 }
